d8cff0f971a7f921b0808f2cd9826a036c50af6f
kazunori-koyama <k-koyama@mtsinc.jp>
2017-11-12 20:14:43 +0900

    Dapperを利用した例を追加。

--- VBWebApplication/Controllers/HomeController.vb
+++ VBWebApplication/Controllers/HomeController.vb
@@ -1,4 +1,6 @@
-﻿Public Class HomeController
+﻿Imports Dapper
+
+Public Class HomeController
     Inherits System.Web.Mvc.Controller
 
     Function Index() As ActionResult
@@ -8,13 +10,22 @@
         Dim record As New m_code
 
         ' レコード追加
-        record.code_bunrui = "新規分類"
-        record.code_value = "1"
-        record.code_text = "分類1"
-        record.start_date = DateTime.Now
+        'record.code_bunrui = "新規分類"
+        'record.code_value = "1"
+        'record.code_text = "分類1"
+        'record.start_date = DateTime.Now
 
-        entities.m_code.Add(record)
-        entities.SaveChanges()
+        'entities.m_code.Add(record)
+        'entities.SaveChanges()
+
+        ' Connection取得
+        Dim list As List(Of m_user) = entities.Database.Connection.Query(Of m_user)("select * from m_user").ToList()
+
+        For Each rec In list
+
+            Console.WriteLine(rec.user_id & ", " & rec.user_name)
+
+        Next
 
         Return View()
     End Function
